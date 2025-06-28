1accbcd6d6ca339171a6b6ce9c9a53d270dedd3d
Dan Marius Larsen <larsendanmarius@gmail.com>
2025-06-28 21:58:51 +0200

    add dashboard & new transaction page with guard

--- src/app/user-dropdown.tsx
+++ src/app/user-dropdown.tsx
@@ -1,18 +1,30 @@
-'use client';
+"use client";
 
-import { UserButton } from '@clerk/nextjs';
+import { UserButton } from "@clerk/nextjs";
+import { ChartColumnBigIcon } from "lucide-react";
+import { useRouter } from "next/navigation";
 
 export default function UserDropdown() {
+  const router = useRouter();
+
   return (
     <UserButton
       showName
       appearance={{
         elements: {
           userButtonOuterIdentifier: {
-            color: 'white',
+            color: "white",
           },
         },
       }}
-    />
+    >
+      <UserButton.MenuItems>
+        <UserButton.Action
+          label="Dashboard"
+          labelIcon={<ChartColumnBigIcon size={16} />}
+          onClick={() => router.push("/dashboard")}
+        />
+      </UserButton.MenuItems>
+    </UserButton>
   );
 }
